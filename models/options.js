bca4e47b2ecf47ed3ad17512266c924ed437777d
skreddykoppula <skreddy.koppula@gmail.com>
2023-01-08 14:48:06 +0530

    trying to add options

--- models/options.js
+++ models/options.js
@@ -2,11 +2,7 @@
 const { Model, Op } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Options extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    
     static createOption({ option, questionId }) {
       let createOption = this.create({
         option,
