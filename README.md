61c6c73f81acdb056774009145dc5e4d3a5fb624
skreddykoppula <skreddy.koppula@gmail.com>
2023-01-08 14:24:48 +0530

    online voating platform

---
+++ README.md
@@ -0,0 +1,19 @@
+ONLINE VOTING APPLICATION
+
+This is a Online Voting Platform built using Node.js which allowselection administrators to sign up and create multiple elections. You can create ballots of multiple questions,create a custom public URL for the election.
+
+The application consists of following features:
+
+An admin should be able to sign up
+    1. If they don’t already have an account, they should be able to sign up first.
+    2.If they are an existing user, they should be able to sign in with their email and password.
+    3.They should also be able to sign out if they'd like to.
+
+Admins can create elections
+    4.Signed-in admins should be able to see a list of elections they have created, and create a new one.
+    5.When creating an election, it should be possible to give a name for the election.
+
+Admins can create a ballot of questions in an election
+    6.Admins should be able to create a ballot - a list of questions they'd like voters to vote on.
+    7.Admins should be able to add multiple questions to the ballot.
+    8.For each question, admins should be able to fill in a short title, and a longer description.
---
+++ __tests__/vote.js
@@ -0,0 +1,84 @@
+
+const request = require("supertest");
+const cheerio = require("cheerio");
+const db = require("../models/index");
+const app = require("../app");
+
+const { response } = require("../app");
+
+let server, agent;
+
+function extractCsrfToken(res) {
+  var $ = cheerio.load(res.text);
+  return $("[name=_csrf]").val();
+}
+
+
+const login = async (agent, username, password) => {
+  let res = await agent.get("/login");
+  let csrfToken = extractCsrfToken(res);
+  res = await agent.post("/session").send({
+    email: username,
+    password: password,
+    _csrf: csrfToken,
+  });
+};
+
+describe("Voting application test suite", function () {
+  beforeAll(async () => {
+    server = app.listen(5000, () => {});
+    agent = request.agent(server);
+  });
+
+  afterAll(async () => {
+    try {
+      await db.sequelize.close();
+      await server.close();
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  test("Signup new user", async () => {
+    res = await agent.get("/signup");
+    const csrfToken = extractCsrfToken(res);
+    res = await agent.post("/admin").send({
+      firstName: "sk",
+      lastName: "reddy",
+      email: "skreddy@gmail.com",
+      password: "skreddy",
+      _csrf: csrfToken,
+    });
+    expect(res.statusCode).toBe(302);
+  });
+
+  test("User login", async () => {
+    res = await agent.get("/electionpage");
+    expect(res.statusCode).toBe(200);
+    await login(agent, "skreddy@gmail.com", "skreddy");
+    res = await agent.get("/electionpage");
+    expect(res.statusCode).toBe(200);
+  });
+
+  test("User signout", async () => {
+    let res = await agent.get("/electionpage");
+    expect(res.statusCode).toBe(200);
+    res = await agent.get("/signout");
+    expect(res.statusCode).toBe(302);
+    res = await agent.get("/electionpage");
+    expect(res.statusCode).toBe(302);
+  });
+
+  test("Creating election", async () => {
+    const agent = request.agent(server);
+    await login(agent, "skreddy@gmail.com", "skreddy");
+    const res = await agent.get("/electionpage/addelection");
+    const csrfToken = extractCsrfToken(res);
+    const response = await agent.post("/electionpage").send({
+      electionName: "election",
+      publicurl: "election",
+      _csrf: csrfToken,
+    });
+    expect(response.statusCode).toBe(302);
+  });
+});
